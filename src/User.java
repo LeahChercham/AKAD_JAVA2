709f7b84d679f90d67f85ad851dc344571983f86
Leah Chercham <leahchercham@hotmail.com>
2021-02-07 18:50:23 +0100

    Created User Class, Constructor, Getter and Setter methods

--- src/User.java
+++ src/User.java
@@ -1,12 +1,58 @@
+/**
+ * The class User creates Instances of the User object.
+ * 
+ * @version 1.0
+ * @author Leah Chercham
+ */
+
 public class User {
     
     // Attribute
-    private String UserID;
+    private Integer UserID;
     private String LastName;
     private String FirstName;
     private String Password;
 
     // Constructor
+    public User(Integer UserID, String LastName, String FirstName, String Password){
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+        this.UserID = UserID;
+        this.Password = Password;
+    }
 
-    // Getter and Setter
+    // Setter & Getter
+    // Setter
+    public void setUserID(Integer UserID) {
+        this.UserID = UserID;
+    }
+
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+
+    // Getter
+    public Integer getUserID() {
+        return UserID;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
 }
