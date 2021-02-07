11bb3edbff0e33d0befcd345b43d6fd0cd35d085
Leah Chercham <leahchercham@hotmail.com>
2021-02-07 13:56:50 +0100

    File separation

---
+++ src/User.java
@@ -0,0 +1,12 @@
+public class User {
+    
+    // Attribute
+    private String UserID;
+    private String LastName;
+    private String FirstName;
+    private String Password;
+
+    // Constructor
+
+    // Getter and Setter
+}
